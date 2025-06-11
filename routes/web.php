dab7ae588a8e76f01d73abf2b63ebd1bb7035b60
fauziaazahra1@gmail.com <fauzia.depriani@mhs.unsoed.ac.id>
2025-06-12 06:17:01 +0700

    nyamain routes

--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,38 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
-use App\Models\Article;
 use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\HistoryController;
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\UserProfileController;
-use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\NotificationController;
+use Illuminate\Notifications\DatabaseNotification;
+use App\Models\User;
+use App\Notifications\PeriodNotification;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {        
+Route::get('/', function () {
     return view('home');
 })->name('/');
 
+// Route::get('/admin/dashboard', function () {
+//     return view('admin.dashboard');
+// })->middleware('role:admin')->name('admin.dashboard');
+
+// Home bisa diakses semua (guest dan user)
+// Route::get('/', [HomeController::class, 'index'])->name('/');
+
+// Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
+//     Route::get('/', [AdminController::class, 'index'])->name('dashboard');
+//     // route admin lainnya ...
+// });
 
 Route::resource('articles', ArticleController::class);
 Route::resource('calendars', CalendarController::class);
 Route::resource('histories', HistoryController::class);
-// Route::get('/calendars', [CalendarController::class, 'index'])->name('calendars.index');
-// Route::post('/histories', [HistoryController::class, 'store']);
-
 Route::resource('profiles', ProfileController::class)->middleware('auth');
-
-Route::get('/profiles/{profile}', [ProfileController::class, 'show'])->name('profiles.show');
-
-// Route::get('/', function () {
-//     $articles = Article::all(); // ambil semua artikel dari database
-//     return view('home', compact('articles')); // kirim ke view
-// });
 
 Route::get('/calendar', function() {
     $events = [];
@@ -36,16 +42,25 @@ Route::get('/calendar', function() {
 });
 
 Route::middleware('auth')->group(function () {
+    Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
+    Route::get('/notifications/{id}', [NotificationController::class, 'detail'])->name('notifications.detail');
+
     Route::get('/profile', [UserProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [UserProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [UserProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Route::get('/test-mail', function () {
-//     \Mail::raw('Test email body', function ($message) {
-//         $message->to('farahtsama@gmail.com')->subject('Test Email');
+// Route::middleware(['auth', 'admin'])->group(function () {
+//     Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+
+
+//     Route::get('/admin/notifications', function () {
+//         $admin = Auth::user();
+//         $notifications = $admin->notifications;
+
+//         return view('admin.notifications', compact('notifications'));
 //     });
-//     return 'Email sent';
+
 // });
 
 require __DIR__.'/auth.php';
